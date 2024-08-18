fabb2ba5ca581aff2e02ee74de361e7df9a7fe45
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-18 23:52:34 +0300

    Update 13 views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -16,7 +16,8 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import  login, authenticate, logout
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
