8aa7d6fdb6bbc1f98c3e088f154eaae90e217d6a
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-19 00:58:11 +0300

    Update 9 urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     # User authentication URLs
-    path('login/', LogoutView.as_view(template_name= 'relationship_app/login.html'), name= 'login'),
+    path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name= 'login'),
     path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name= 'logout'),
     path('register/', views.register.as_view(), name='register'),
 
