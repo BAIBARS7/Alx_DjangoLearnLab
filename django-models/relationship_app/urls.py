8fdd1ff734243918445c1817048a3c1cfea793a6
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-18 23:13:33 +0300

    Update 4 urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('books/add/', add_book, name="add_book"),
     path('books/<int:pk>/edit/', edit_book, name="edit_book"),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('books/<int:pk>/delete/', delete_book, name="delete_book"),
 
     # Library detail URL
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
