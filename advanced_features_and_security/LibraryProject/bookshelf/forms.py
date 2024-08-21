b7769fc8298b6757ff72bfb4f64b9ade824861af
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-21 21:02:11 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,7 @@
+from django import forms
+from .models import Book  # Assuming you're using the Book model for this example
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date', 'isbn', 'summary']
