a8b952994e2f97660ff97be2dc61e8f056ad524c
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-21 21:08:02 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -31,3 +31,16 @@ def delete_book(request, book_id):
 # CSRF and session cookies are secured to only be transmitted over HTTPS
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+from .forms import ExampleForm
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            return redirect('success_view')  # Redirect to a success page after submission
+    else:
+        form = ExampleForm()  # Instantiate an empty form for GET requests
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
