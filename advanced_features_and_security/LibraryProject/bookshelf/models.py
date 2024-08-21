0b67d687c94bb7815d7d9182f2a5905a840d9877
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-21 20:12:01 +0300

    Update 1 models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,6 +9,23 @@ class Book(models.Model):
         return self.title
 
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, date_of_birth=None, profile_photo=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password=password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
