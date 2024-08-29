8157ad9156241548650d8fb4b6f3bd468e07f28a
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-08-29 17:44:01 +0300

    Update 4 views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
