8dfde8983e2eb11820c1cf6436881ec4e879dc1c
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-09-05 17:09:28 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
