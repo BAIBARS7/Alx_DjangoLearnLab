c9302c85e43511d41a066a4bda2c9379c625d44e
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-09-05 19:11:31 +0300

    Update 8 views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
